e97cc865502ecd741c64cf00a0c078f35c7a34d3
Loving Programming <officialstanlee143@gmail.com>
2026-01-16 23:56:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,24 @@
 module.exports = {
+    // --- 1. BOT IDENTITY ---
     botName: "WRONG TURN 6",
     ownerName: "STANYTZ",
     ownerNumber: "255618558502", 
-    prefix: ".",
-    mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority",
+    prefix: ".", 
+
+    // --- 2. MEDIA ASSETS ---
+    menuImage: "https://files.catbox.moe/07uq2a.jpg",
+
+    // --- 3. SOCIAL HUB LINKS ---
     channelLink: "https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p",
     groupLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
-    groupId: "120363302194515518@g.us", // Verify this JID
-    menuImage: "https://i.ibb.co/vz6mD7y/wrongturn.jpg"
+    
+    // --- 4. SECURITY & LOCKDOWN ---
+    groupId: "120363302194515518@g.us", 
+
+    // --- 5. SYSTEM SETTINGS ---
+    autoStatusView: true,
+    autoStatusLike: true,
+    antiLink: true,
+    antiDelete: true,
+    alwaysOnline: true
 };
