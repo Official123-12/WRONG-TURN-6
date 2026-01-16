6533b938382b769d849c2cf651c01e80aa925b78
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 18:11:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
     ownerName: "STANYTZ",
     ownerNumber: "255618558502", 
     prefix: ".",
-    mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority&appName=Cluster0",
+    mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority",
     channelLink: "https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p",
     groupLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
-    groupId: "120363302194515518@g.us", // Use .jid command to verify
+    groupId: "120363302194515518@g.us", 
     menuImage: "https://i.ibb.co/vz6mD7y/wrongturn.jpg"
 };
