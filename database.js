ec9bd1cc1ca668c3a9625a7857099986bb32dfe9
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 10:54:36 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+
+const SessionSchema = new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    creds: { type: Object } // Hapa ndipo login ya WhatsApp itakaa
+});
+
 const UserSchema = new mongoose.Schema({
     id: { type: String, unique: true },
-    name: String,
     antiDelete: { type: Boolean, default: true },
     antiViewOnce: { type: Boolean, default: true },
-    autoTyping: { type: Boolean, default: true },
-    autoRecording: { type: Boolean, default: true },
-    alwaysOnline: { type: Boolean, default: true }
+    verified: { type: Boolean, default: false }
 });
-const User = mongoose.model('WT6_User', UserSchema);
-module.exports = { User };
+
+const Session = mongoose.model('Session', SessionSchema);
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { Session, User };
