eff6be4b9e5fb99c5e0c4f379a897d35c71d5a4c
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 22:18:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority",
     channelLink: "https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p",
     groupLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
-    groupId: "120363302194515518@g.us", 
+    groupId: "120363302194515518@g.us", // Verify this JID
     menuImage: "https://i.ibb.co/vz6mD7y/wrongturn.jpg"
 };
