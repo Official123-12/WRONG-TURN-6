f41b6138196b01ef4be540f3684a3e393a835307
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 03:49:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
     ownerName: "STANYTZ",
     ownerNumber: "255618558502", 
     prefix: ".",
-    // Use the link we verified
-    mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority&appName=Cluster0",
+    mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority",
     channelLink: "https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p",
     groupLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
+    groupId: "J19JASXoaK0GVSoRvShr4Y@g.us", // Your Group JID
     menuImage: "https://files.catbox.moe/jdtfqg.jpeg"
 };
