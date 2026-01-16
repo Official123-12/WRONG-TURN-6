3c317c13c2110add5acb97806c0f983ffb8b4576
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 21:29:55 +0300

    Update translate.js

--- commands/intellect/translate.js
+++ commands/intellect/translate.js
@@ -1,1 +1,13 @@
-
+const axios = require("axios");
+module.exports = {
+    name: "translate",
+    async execute(sock, msg, args) {
+        const lang = args[0];
+        const text = args.slice(1).join(" ");
+        if (!lang || !text) return sock.sendMessage(msg.key.remoteJid, { text: "Use: .translate en Habari" });
+        try {
+            const res = await axios.get(`https://api.popcat.xyz/translate?to=${lang}&text=${encodeURIComponent(text)}`);
+            await sock.sendMessage(msg.key.remoteJid, { text: `🌍 *GLOBAL TRANSLATOR*\n\n*Result:* ${res.data.translated}` });
+        } catch (e) { await sock.sendMessage(msg.key.remoteJid, { text: "Translation failed!" }); }
+    }
+};
